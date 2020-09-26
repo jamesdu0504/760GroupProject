7560be93bf439d7f0aec2a160138f62d67358e71
mrog173 <59722147+mrog173@users.noreply.github.com>
2020-09-26 17:51:15 +1200

    Working on CHARM

--- test_closed_itemset.py
+++ test_closed_itemset.py
@@ -16,62 +16,64 @@ def get_closed_itemsets_new(baskets, threshold):
                 sorted_p[row["itemsets"]] = row["support"]
 
     #Sort
-    P = []
+    P = [] #Frequent single items ordered by f
     for i in sorted(sorted_p.items(), key=lambda x: x[1], reverse=True):
         P += [i[0]]
     
-    T = {}
-    #Find transactions that each occur in and store as a list?
-    for column in baskets:
-        T[column] = baskets.index[baskets[column]].tolist()
-
-
-    print(T)
+    T = {} #Tidset indexed by P_i
+    #Find transactions that each of P occur in and store as a list
+    for P_i in P:
+        T[P_i] = set(baskets.index[baskets[P_i]])
 
     #Call recursive part
-    #closed_itemsets = charm_extended(freq, P, threshold, dict())
+    closed_itemsets = charm_extended(freq, P, threshold, dict())
     #return closed_itemsets, itemsets
 
-# def charm_extended(freq, P, min_sup):
-#     for Xi in P:                                #For Xi x t(Xi) in P
-#         Pi = set()                              #Empty set,
-#         for Xj in P:                            #For Xj >= f Xi
-#             X = Xi.union(Xj)
-#             Y = t(Xi).intersection(t(Xj))
+def charm_extended(freq, P, min_sup, C, T):
+    for i, Xi in enumerate(P):                  #For Xi x t(Xi) in P
+        Pi = {}
+        for Xj in P[i+1:]:                      #For Xj >= f Xi
+            X = Xi.union(Xj)
+            Y = T[Xj].intersection(T[Xj])       #Tidset combining these
 
-#             #Should be finding the support of X and comparing to the threshold
-#             if freq.loc[freq['itemsets'] == X].iloc[0]["support"] >= min_sup:
-#                 if t(Xi) == t(Xj):              #Property 1
-#                     P.discard(Xj)               #Remove Xj from P
-#                     replaceInItems(Xi,          #Replace Xi with X
+            #Should be finding the support of X and comparing to the threshold
+            if freq.loc[freq['itemsets'] == X].iloc[0]["support"] >= min_sup:
+                if T[Xi] == T[Xj]:              #Property 1
+                    print("Property 1")
+                    P.discard(Xj)               #Remove Xj from P
+                    replaceInItems(Xi,X)        #Replace Xi with X
 
-#                 elif t(Xi).issubset(t(Xj)):     #Property 2
-#                     #Replace all Xi with X
+                elif T[Xi].issubset(T[Xj]):     #Property 2
+                    print("Property 2")
+                    replaceInItems(Xi,X)        #Replace all Xi with X
 
-#                 elif t(Xj).issuperset(t(Xj)):   #Property 3
-#                     P = P.discard(Xj)           #Remove Xj from P
-#                     #Add X x Y to [Pi] using ordering f
+                elif T[Xi].issuperset(T[Xj]):   #Property 3
+                    print("Property 3")
+                    P = P.discard(Xj)           #Remove Xj from P
+                    Pi.add(X)                   #TODO: Add X x Y to [Pi] using ordering f
 
-#                 elif t(Xi) != t(Xj):            #Property 4
-#                     #Add X x Y to [Pi] using ordering f
+                elif T[Xi] != T[Xj]:            #Property 4
+                    print("Property 4")
+                                                #TODO: Add X x Y to [Pi] using ordering f
 
-#         if Pi != {}:
-#             C = charm_extended(freq, Pi, min_sup, C)
-#         del Pi
-#         cl.append((X, row['support']))
-#     return C
+        if Pi != {}:
+            C = charm_extended(freq, Pi, min_sup, C) #TODO: is it passing everything correctly
+        del Pi                                  #TODO: what are we deleting?
+        C.append((X, row['support']))
+    return C
 
-# def replaceInItems(current, target, set_a):
-#     #Replace Xi with X in A
-#     for key in set_a.keySet():
-#         if key.contains(curr):
-#             temp.add(key)
+def replaceInItems(current, target, set_a):
+    #TODO: replace occurances of current with target in set_a
+    #Replace Xi with X in A
+    for key in set_a.keySet():
+        if key.contains(curr):
+            temp.add(key)
 
-#     for key in temp:
-#         val = set_a.get(key)
-#         set_a.remove(key)
-#         key.addAll(target)
-#         set_a.put(key, val)
+    for key in temp:
+        val = set_a.get(key)
+        set_a.remove(key)
+        key.addAll(target)
+        set_a.put(key, val)
 
 
 def main():
