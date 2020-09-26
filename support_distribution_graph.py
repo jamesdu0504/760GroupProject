3f8fa280bdb6a290d4b9a5778bf08fb96d9563f8
mrog173 <59722147+mrog173@users.noreply.github.com>
2020-09-26 14:49:59 +1200

    Fixed support graph plotting

--- support_distribution_graph.py
+++ support_distribution_graph.py
@@ -5,50 +5,93 @@ import matplotlib.pylab as plt
 from mlxtend.frequent_patterns import apriori, fpgrowth
 from mlxtend.frequent_patterns import association_rules
 
-#This creates a support graph for each dataset
-#Plot between frequent itemsets > 0 and frequent itemsets < 50,000
-#Very slow code, wouldn't suggest trying it
+import random
 
-datasets = ["BMS1", 
-            "BMS2", 
-            "toydata",
-            "uci_retail",
-            "mushroom",
-            "Belgian_retail",
-            "chess", 
-            "connect", 
-            "mushroom", 
-            "pumsb", 
-            "pumsb_star", 
-            "T40I10D100K", 
-            "T10I4D100K", 
-            "accidents", 
-            "instacart"]
+def support_graph_distribution(frequent_itemsets, min_support, name):
+    #Draw the frequent itemsets distribution
+    # frequent_itemsets: original frequent itemsets mined with support > min_support
+    # name: name of the dataset
+    plt.title("Number of Frequent Itemsets for Varying Levels of \nSupport for the " + name + " Dataset.")
+    plt.hist(frequent_itemsets['support'], 
+             bins=300, 
+             density=True, 
+             histtype='bar', 
+             cumulative=-1, 
+             range=(max(min_support, 0), frequent_itemsets['support'].max()),
+             color="dodgerblue")
 
-def main(dataset):
-    y = []
-    x = []
-
-    print("Processing:", dataset)
-    basket_sets = im.import_dataset(dataset) 
-    for min_support in range(200, 0, -1):
-        frequent_itemsets = fpgrowth(basket_sets, min_support=0.005*min_support, use_colnames=True)
-        if len(frequent_itemsets)>50000:
-            break
-        if len(frequent_itemsets) != 0:
-            if len(x) == 0:
-                x += [0.005*(min_support+1)]
-                y += [0]
-            y += [len(frequent_itemsets)]
-            x += [0.005*min_support]
-
-    plt.title("Number of Frequent Itemsets for Varying Levels of \nSupport for the " + dataset + " Dataset.")
-    plt.plot(x, y, label='test_label1', color="turquoise")
     plt.ylabel("Number of frequent itemsets")
     plt.xlabel("Support threshold")
-    plt.fill_between(x, y, color="paleturquoise")
-    plt.savefig("supports/"+dataset+".png")
+    plt.savefig("supports/"+name+".png")
     plt.close()
 
-for dataset in datasets:
-    main(dataset)
+
+def dual_support_graph_distribution(frequent_itemsets_A, frequent_itemsets_B, min_support, name):
+    #Draw the sanitized and unsanitized frequent itemsets distributions over each other
+    # frequent_itemsets_A: original frequent itemsets mined with support > min_support
+    # frequent_itemsets_B: sanitised frequent itemsets mined with support > min_support
+    # name: name of the dataset
+
+    plt.title("Number of Frequent Itemsets for Varying Levels of \nSupport for the original and sanitised " + name + " Dataset.")
+    plt.hist(frequent_itemsets_A['support'], 
+             bins=300, 
+             density=False, 
+             histtype='bar', 
+             cumulative=-1, 
+             range=(max(min_support, 0), frequent_itemsets_A['support'].max()),
+             color="dodgerblue",
+             label="Original")
+
+    plt.hist(frequent_itemsets_B['support'], 
+             bins=300, 
+             density=False, 
+             histtype='bar', 
+             cumulative=-1, 
+             range=(max(min_support, 0), frequent_itemsets_B['support'].max()),
+             color="limegreen",
+             alpha=0.8,
+             label="Sanitised")
+
+    plt.legend()
+
+    plt.ylabel("Number of frequent itemsets")
+    plt.xlabel("Support threshold")
+    plt.savefig("supports/"+name+"_dual.png")
+    plt.close()
+
+
+#Manually assigning minimum supports
+datasets = {"toydata": 0.005,
+            "BMS1": 0.00085, 
+            "BMS2": 0.0005, 
+            "uci_retail": 0.005,
+            "mushroom": 0.1,
+            "Belgian_retail": 0.0005,
+            "chess": 0.7, 
+            "connect": 0.8, 
+            "pumsb": 0.83, 
+            "pumsb_star": 0.38, 
+            "T40I10D100K": 0.001, 
+            "T10I4D100K": 0.011, 
+            "accidents": 0.38, 
+            "instacart": 0.005}
+
+def main(dataset, min_sup):
+    print("Processing:", dataset)
+    basket_sets = im.import_dataset(dataset)
+    
+    #Plot the support distribution
+    frequent_itemsets = fpgrowth(basket_sets, min_support=min_sup, use_colnames=True)
+    support_graph_distribution(frequent_itemsets, min_sup, dataset)
+
+    #Example of plotting the dual distributions
+    #Plot the dual distribution by randomly reducing some values for testing
+    copy = frequent_itemsets.copy()
+    copy.dropna(inplace=True) #This is needed for some reason?
+    for i in range(copy.shape[0]//2):
+        copy.loc[random.randint(0, copy.shape[0]), ["support"]] = copy.loc[random.randint(0, copy.shape[0]-1), ["support"]]/2
+
+    dual_support_graph_distribution(frequent_itemsets, copy, min_sup, dataset)
+
+for key, value in datasets.items():
+    main(key, value)
