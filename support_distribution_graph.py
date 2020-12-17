8f435baf264493e924894ae60f4fcc3d8990271a
Mitchell Rogers <mrog173@aucklanduni.ac.nz>
2020-12-17 20:03:13 +1300

    Updated graphs
    - SVG output
    - No title
    - Saves graph data for analysis

--- support_distribution_graph.py
+++ support_distribution_graph.py
@@ -11,7 +11,7 @@ def support_graph_distribution(frequent_itemsets, min_support, name):
     #Draw the frequent itemsets distribution
     # frequent_itemsets: original frequent itemsets mined with support > min_support
     # name: name of the dataset
-    plt.title("Number of Frequent Itemsets for Varying Levels of \nSupport for the " + name + " Dataset.")
+    #plt.title("Number of Frequent Itemsets for Varying Levels of \nSupport for the " + name + " Dataset.")
     plt.hist(frequent_itemsets['support'], 
              bins=300, 
              density=False, 
@@ -22,7 +22,7 @@ def support_graph_distribution(frequent_itemsets, min_support, name):
 
     plt.ylabel("Number of frequent itemsets")
     plt.xlabel("Support threshold")
-    plt.savefig("supports/"+name+".png")
+    plt.savefig("supports/"+name+".svg", format="svg")
     plt.close()
 
 
@@ -32,7 +32,7 @@ def dual_support_graph_distribution(frequent_itemsets_A, frequent_itemsets_B, mi
     # frequent_itemsets_B: sanitised frequent itemsets mined with support > min_support
     # name: name of the dataset
 
-    plt.title("Number of Frequent Itemsets for Varying Levels of \nSupport for the original and sanitised " + name + " Dataset.")
+    #plt.title("Number of Frequent Itemsets for Varying Levels of \nSupport for the original and sanitised " + name + " Dataset.")
     plt.hist(frequent_itemsets_A['support'], 
              bins=300, 
              density=False, 
@@ -52,33 +52,17 @@ def dual_support_graph_distribution(frequent_itemsets_A, frequent_itemsets_B, mi
              alpha=0.8,
              label="Sanitised")
 
+    #Save the data for reconstruction later
+    frequent_itemsets_B.to_csv("frequent_itemsets/FI_"+name+".csv")
+
     plt.legend()
 
     plt.ylabel("Number of frequent itemsets")
     plt.xlabel("Support threshold")
-    plt.savefig("supports/"+name+"_dual.png")
+    plt.savefig("supports/"+name+"_dual.svg", format="svg")
     plt.close()
 
 def main(dataset, min_sup):
-    #Manually assigning minimum supports
-    datasets = {"toydata": 0.005,
-                "BMS1": 0.00085, 
-                "BMS2": 0.0005, 
-                "uci_retail": 0.005,
-                "mushroom": 0.1,
-                "Belgian_retail": 0.0005,
-                "chess": 0.7, 
-                "connect": 0.8, 
-                "pumsb": 0.83, 
-                "pumsb_star": 0.38, 
-                "T40I10D100K": 0.011, 
-                "T10I4D100K": 0.001, 
-                "accidents": 0.38, 
-                "instacart": 0.005}
-
-    for key, value in datasets.items():
-        main(key, value)
-
     print("Processing:", dataset)
     basket_sets = im.import_dataset(dataset)
     
@@ -95,3 +79,21 @@ def main(dataset, min_sup):
 
     # dual_support_graph_distribution(frequent_itemsets, copy, min_sup, dataset)
 
+#Manually assigning minimum supports
+# datasets = {"toydata": 0.005,
+#             "BMS1": 0.00085, 
+#             "BMS2": 0.0005, 
+#             "uci_retail": 0.005,
+#             "mushroom": 0.1,
+#             "Belgian_retail": 0.0005,
+#             "chess": 0.7, 
+#             "connect": 0.8, 
+#             "pumsb": 0.83, 
+#             "pumsb_star": 0.38, 
+#             "T40I10D100K": 0.011, 
+#             "T10I4D100K": 0.001, 
+#             "accidents": 0.38, 
+#             "instacart": 0.005}
+
+# for key, value in datasets.items():
+#     main(key, value)
